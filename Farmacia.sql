152640103d7a934c8ff98f50ac702d4fbb111fbb
DaihanaInsfran <167359024+DaihanaInsfran@users.noreply.github.com>
2025-04-09 18:26:19 -0400

    Update Farmacia.sql

--- Farmacia.sql
+++ Farmacia.sql
@@ -1,111 +1,130 @@
 
--- Creación de la base de datos farmacia
+-- CreaciÃ³n de la base de datos farmacia
 CREATE DATABASE farmacia;
 
 -- Usar la base de datos farmacia
 USE farmacia;
-
-
--- Creación de la tabla laboratorio
-CREATE TABLE laboratorio (
-    id_laboratorio INTEGER PRIMARY KEY,
-    lab_descripcion VARCHAR(255),
-    lab_telefono VARCHAR(50),
-    lab_direccion VARCHAR(255)
-);
-
--- Creación de la tabla producto
-CREATE TABLE producto (
-    id_producto INTEGER PRIMARY KEY,
-    pro_descripcion VARCHAR(255),
-    pro_precio INTEGER,
-    pro_cantidad INTEGER,
-    pro_vencimiento TIMESTAMP,
-    id_laboratorio INTEGER,
-    FOREIGN KEY (id_laboratorio) REFERENCES laboratorio(id_laboratorio)
-);
-
--- Creación de la tabla role
-CREATE TABLE role (
-    id_role INTEGER PRIMARY KEY,
-    role_descripcion VARCHAR(255)
-);
-
--- Creación de la tabla modulo
 CREATE TABLE modulo (
-    id_modulo INTEGER PRIMARY KEY,
-    mod_descripcion VARCHAR(255)
+    id_modulo INT IDENTITY(1,1) NOT NULL,
+    mod_url NVARCHAR(MAX) NOT NULL,
+    mod_titulo VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id_modulo)
 );
 
--- Creación de la tabla permiso
+CREATE TABLE role (
+    id_role INT IDENTITY(1,1) NOT NULL,
+    rol_descripcion VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id_role)
+);
+
 CREATE TABLE permiso (
-    id_modulo INTEGER,
-    id_role INTEGER,
+    id_modulo INT NOT NULL,
+    id_role INT NOT NULL,
     PRIMARY KEY (id_modulo, id_role),
-    FOREIGN KEY (id_modulo) REFERENCES modulo(id_modulo),
-    FOREIGN KEY (id_role) REFERENCES role(id_role)
+    FOREIGN KEY (id_modulo) REFERENCES modulo (id_modulo),
+    FOREIGN KEY (id_role) REFERENCES role (id_role)
 );
 
--- Creación de la tabla persona
-CREATE TABLE persona (
-    id_persona INTEGER PRIMARY KEY,
-    per_nombre VARCHAR(255),
-    per_cedula VARCHAR(20),
-    per_correo VARCHAR(100),
-    per_nic VARCHAR(50),
-    per_telefono VARCHAR(50),
-    per_usuario VARCHAR(50),
-    per_clave VARCHAR(100),
-    id_role INTEGER,
-    FOREIGN KEY (id_role) REFERENCES role(id_role)
+CREATE TABLE producto (
+    id_producto INT IDENTITY(1,1) NOT NULL,
+    pro_descripcion VARCHAR(50) NOT NULL,
+    pro_cantidad INT NOT NULL,
+    pro_precio INT NOT NULL,
+    PRIMARY KEY (id_producto)
 );
 
--- Creación de la tabla proveedor
-CREATE TABLE proveedor (
-    id_proveedor INTEGER PRIMARY KEY,
-    pro_descripcion VARCHAR(255)
+CREATE TABLE empleado (
+    id_empleado INT IDENTITY(1,1) NOT NULL,
+    emp_cedula VARCHAR(50) NOT NULL,
+    emp_nombre VARCHAR(100) NOT NULL,
+    emp_celular VARCHAR(50) NOT NULL,
+    emp_usuario VARCHAR(50) NOT NULL,
+    emp_clave VARCHAR(50) NOT NULL,
+    emp_estado VARCHAR(20) NOT NULL,
+    id_role INT NOT NULL,
+    PRIMARY KEY (id_empleado),
+    FOREIGN KEY (id_role) REFERENCES role (id_role),
+    CHECK (emp_estado IN ('Activo', 'Vacaciones', 'Inactivo'))
 );
 
--- Creación de la tabla compra
+CREATE TABLE horario (
+    id_horario INT IDENTITY(1,1) NOT NULL,
+    id_empleado INT NOT NULL,
+    hor_dia VARCHAR(20) NOT NULL,
+    hor_inicio TIME NOT NULL,
+    hor_fin TIME NOT NULL,
+    PRIMARY KEY (id_horario),
+    FOREIGN KEY (id_empleado) REFERENCES empleado (id_empleado),
+    CHECK (hor_dia IN ('Lunes', 'Martes', 'MiÃ©rcoles', 'Jueves', 'Viernes', 'SÃ¡bado', 'Domingo'))
+);
+
 CREATE TABLE compra (
-    id_compra INTEGER PRIMARY KEY,
-    com_total INTEGER,
-    com_fecha TIME,
-    com_factura VARCHAR(50),
-    id_persona INTEGER,
-    id_proveedor INTEGER,
-    FOREIGN KEY (id_persona) REFERENCES persona(id_persona),
-    FOREIGN KEY (id_proveedor) REFERENCES proveedor(id_proveedor)
+    id_compra INT IDENTITY(1,1) NOT NULL,
+    id_empleado INT NOT NULL,
+    com_factura VARCHAR(50) NOT NULL,
+    com_fecha DATETIME NOT NULL,
+    com_total INT NOT NULL,
+    PRIMARY KEY (id_compra),
+    FOREIGN KEY (id_empleado) REFERENCES empleado (id_empleado)
 );
 
--- Creación de la tabla compra_detalle
-CREATE TABLE compra_detalle (
-    id_compra_detalle INTEGER PRIMARY KEY,
-    comdet_cantidad INTEGER,
-    comdet_subtotal INTEGER,
-    id_producto INTEGER,
-    id_compra INTEGER,
-    FOREIGN KEY (id_producto) REFERENCES producto(id_producto),
-    FOREIGN KEY (id_compra) REFERENCES compra(id_compra)
+CREATE TABLE compradetalle (
+    id_compradetalle INT IDENTITY(1,1) NOT NULL,
+    id_compra INT NOT NULL,
+    id_producto INT NOT NULL,
+    comdet_cantidad INT NOT NULL,
+    comdet_subtotal INT NOT NULL,
+    comdet_vencimiento VARCHAR(50) NOT NULL, -- Varchar porque los medicamentos no dan dia para el vencimiento, asi que quedaria 12-2025 en lugar de 29-12-2025
+    PRIMARY KEY (id_compradetalle),
+    FOREIGN KEY (id_compra) REFERENCES compra (id_compra),
+    FOREIGN KEY (id_producto) REFERENCES producto (id_producto)
 );
 
--- Creación de la tabla venta
+CREATE TABLE cliente (
+    id_cliente INT IDENTITY(1,1) NOT NULL,
+    cli_cedula VARCHAR(50) NOT NULL,
+    cli_nombre VARCHAR(100) NOT NULL,
+    cli_nacimiento DATE NOT NULL,
+    cli_celular VARCHAR(50) NOT NULL,
+    cli_correo VARCHAR(50) NOT NULL,
+    cli_direccion VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id_cliente)
+);
+
+CREATE TABLE cita (
+    id_cita INT IDENTITY(1,1) NOT NULL,
+    id_cliente INT NOT NULL,
+    id_empleado INT NOT NULL,
+    cit_fecha TIME NOT NULL,
+    cit_estado VARCHAR(20) DEFAULT 'Pendiente',
+    cit_pago INT NOT NULL,
+    cit_nota NVARCHAR(MAX),
+    PRIMARY KEY (id_cita),
+    FOREIGN KEY (id_cliente) REFERENCES cliente (id_cliente),
+    FOREIGN KEY (id_empleado) REFERENCES empleado (id_empleado),
+    CHECK (cit_estado IN ('Pendiente', 'Confirmada', 'Cancelada', 'Completada'))
+);
+
 CREATE TABLE venta (
-    id_venta INTEGER PRIMARY KEY,
-    ven_total INTEGER,
-    ven_fecha TIME,
-    ven_factura VARCHAR(50),
-    id_persona INTEGER,
-    FOREIGN KEY (id_persona) REFERENCES persona(id_persona)
+    id_venta INT IDENTITY(1,1) NOT NULL,
+    id_cliente INT NOT NULL,
+    id_empleado INT NOT NULL,
+    ven_fecha DATETIME NOT NULL,
+    ven_total INT NOT NULL,
+    ven_factura VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id_venta),
+    FOREIGN KEY (id_cliente) REFERENCES cliente (id_cliente),
+    FOREIGN KEY (id_empleado) REFERENCES empleado (id_empleado)
 );
 
--- Creación de la tabla venta_detalle
-CREATE TABLE venta_detalle (
-    id_venta_detalle INTEGER PRIMARY KEY,
-    vendet_cantidad INTEGER,
-    vendet_subtotal INTEGER,
-    id_producto INTEGER,
-    id_venta INTEGER,
-    FOREIGN KEY (id_producto) REFERENCES producto(id_producto),
-    FOREIGN KEY (id_venta) REFERENCES venta(id_venta)
+CREATE TABLE ventadetalle (
+    id_ventadetalle INT IDENTITY(1,1) NOT NULL,
+    id_venta INT NOT NULL,
+    id_producto INT NOT NULL,
+    vendet_cantidad INT NOT NULL,
+    vendet_subtotal INT NOT NULL,
+    PRIMARY KEY (id_ventadetalle),
+    FOREIGN KEY (id_venta) REFERENCES venta (id_venta),
+    FOREIGN KEY (id_producto) REFERENCES producto (id_producto)
 );
+
